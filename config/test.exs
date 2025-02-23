e7aa7bcabb23c11657887651d79e5b1c991cda10
Avi Kav <avikav@avikav.net>
2025-02-23 03:38:09 -0500

    `mix phx.new`

---
+++ config/test.exs
@@ -0,0 +1,28 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :avikav_net, AvikavNet.Repo,
+  database: Path.expand("../avikav_net_test.db", __DIR__),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :avikav_net, AvikavNetWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "gJnGLVZ07Z5OM5jY8KxHOlDV1jU7NquGPyiyyKsCHfO7K3mL6pCwIaVoC1b9n2l3",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
+
+# Enable helpful, but potentially expensive runtime checks
+config :phoenix_live_view,
+  enable_expensive_runtime_checks: true
---
+++ test/avikav_net_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule AvikavNetWeb.ErrorJSONTest do
+  use AvikavNetWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert AvikavNetWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert AvikavNetWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
