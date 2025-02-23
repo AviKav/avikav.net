ebdb95db96d9136964cfea85fc8d69e8807aa21b
Avi Kav <avikav@avikav.net>
2025-02-23 03:55:28 -0500

    Sentry

--- config/config.exs
+++ config/config.exs
@@ -49,6 +49,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :avikav_net, :logger, [
+  {:handler, :avikav_net_sentry_handler, Sentry.LoggerHandler,
+   %{
+     config: %{metadata: [:file, :line]}
+   }}
+]
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
